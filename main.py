a9469c35b527f6ee065b3f43282bebaad6aa8bcf
cirno <cirnoqvq@163.com>
2024-08-30 15:56:35 +0800

    update: minimum travel time for each level

--- main.py
+++ main.py
@@ -39,7 +39,19 @@ def try4distance(start, end):
     }
     data["features"][0]["geometry"]["coordinates"] = start
     data["features"][1]["geometry"]["coordinates"] = end
-    response = requests.post(base, data=json.dumps(data), headers=headers).json()
+
+    # response = requests.post(base, data=json.dumps(data), headers=headers).json()
+
+    # try 5 times
+    response = None
+    for _ in range(5):
+        try:
+            response = requests.post(base, data=json.dumps(data), headers=headers).json()
+            break
+        except Exception as e:
+            print_and_log(e)
+            time.sleep(1)
+
     cost = response["features"][0]["properties"]["weight"]
     return cost
 
@@ -49,6 +61,7 @@ def try4distance(start, end):
 # ---Gussian(with different thresholds)---
 def gussian_w(t, thresholds):
     # if t > 60:
+        # return 0
     # return (math.exp(-1 / 2 * (t / 60) ** 2) - math.exp(-1 / 2)) / (1 - math.exp(-1 / 2))
     if t > thresholds:
         return 0
@@ -115,23 +128,26 @@ def reindex_S1():
     S1Rj.to_csv("./data/tmp/S1Rj_reindex.csv", index=False)
 
 # S2, process a hospital
-@ray.remote
-def process_hospital(hospital, start, thresholds):
+@ray.remote(num_returns=3)
+def process_hospital(hospital, start):
     end = [hospital["lng"], hospital["lat"]]
+    thresholds = 60 if hospital["grade"].startswith("Level3") else 45 if hospital["grade"].startswith("Level2") else 30
     cost = try4distance(start, end)
     if cost == "no path found":
-        return 0
+        return [0.0, -1.0, thresholds]
+    
     cost = float(cost)
-
-    thresholds = 60 if hospital["grade"].startswith("Level3") else 45 if hospital["grade"].startswith("Level2") else 30
-
     RW = hospital["R"] * gussian_w(cost, thresholds)
-    return 0 if RW == 0 else RW
+    return [0.0 if RW == 0 else RW, cost, thresholds]
 
 # S2, process a grid
 @ray.remote
 def process_grid_i(i, grid, length):
     grid["acc"] = 0.0
+    grid["nearest_3"] = -1.0
+    grid["nearest_2"] = -1.0
+    grid["nearest_1"] = -1.0
+
     centroid = grid.geometry.centroid
     start = [centroid.x, centroid.y]
 
@@ -145,12 +161,23 @@ def process_grid_i(i, grid, length):
         for j in range(len(filtered_hospitals))
     ]
     for handle in result_handles:
-        grid["acc"] += ray.get(handle)
+        RW, cost, thresholds = ray.get(handle)
+        grid["acc"] += RW
+
+        if thresholds == 60:
+            if grid["nearest_3"] == -1 or grid["nearest_3"] > cost:
+                grid["nearest_3"] = cost
+        elif thresholds == 45:
+            if grid["nearest_2"] == -1 or grid["nearest_2"] > cost:
+                grid["nearest_2"] = cost
+        elif thresholds == 30:
+            if grid["nearest_1"] == -1 or grid["nearest_1"] > cost:
+                grid["nearest_1"] = cost
 
     if i % 10000 == 0:
         print_and_log(f"S2, {i}/{length}")
 
-    return f"{i},{grid['acc']}\n"
+    return f"{i},{grid['acc']},{grid['nearest_3']},{grid['nearest_2']},{grid['nearest_1']}\n"
 
 # S2, reindex and merge
 def reindex_acc(region_name, grids):
@@ -168,7 +195,7 @@ def reindex_acc(region_name, grids):
 
 
 # process a region
-def main(region_name, shape):
+def process_region(region_name, shape):
     # --- init ---
     global grids
     global grids_id
@@ -182,11 +209,9 @@ def main(region_name, shape):
 
     check_and_remove("./data/tmp/S1Rj.csv")
     check_and_remove("./data/tmp/acc.csv")
-    check_and_remove("./data/tmp/S1Rj_reindex.csv")
-    check_and_remove("./data/tmp/acc_reindex.csv")
 
     check_and_create("./data/tmp/S1Rj.csv", "id,R\n")
-    check_and_create("./data/tmp/acc.csv", "index,acc\n")
+    check_and_create("./data/tmp/acc.csv", "index,acc,nearest_3,nearest_2,nearest_1\n")
 
     hospitals = pd.read_csv(hospitals_path)
     hospitals_gdf = gpd.GeoDataFrame(hospitals, geometry=gpd.points_from_xy(hospitals["lng"], hospitals["lat"]))
@@ -195,37 +220,40 @@ def main(region_name, shape):
     # --- init ---
 
     # --- Step 1 ---
-    print_and_log("Step 1 start...")
     time1 = time.time()
-    grids_id = ray.put(grids)
+    if not os.path.exists(f"./data/tmp/S1Rj_reindex.csv"):
+        print_and_log("Step 1 start...")
+        grids_id = ray.put(grids)
 
-    hospitals_length = len(hospitals_gdf)
-    batches = [
-        hospitals_gdf.iloc[i : i + S1_batch_size]
-        for i in range(0, hospitals_length, S1_batch_size)
-    ]
-    exists = 0
-    for batch in batches:
-        res = ray.get(
-            [
-                process_hospital_i.remote(i + exists, batch.iloc[i], hospitals_length)
-                for i in range(len(batch))
-            ]
-        )
-        with open("./data/tmp/S1Rj.csv", "a") as f:
-            for r in res:
-                f.write(r)
-        exists += len(batch)
+        hospitals_length = len(hospitals_gdf)
+        batches = [
+            hospitals_gdf.iloc[i : i + S1_batch_size]
+            for i in range(0, hospitals_length, S1_batch_size)
+        ]
+        exists = 0
+        for batch in batches:
+            res = ray.get(
+                [
+                    process_hospital_i.remote(i + exists, batch.iloc[i], hospitals_length)
+                    for i in range(len(batch))
+                ]
+            )
+            with open("./data/tmp/S1Rj.csv", "a") as f:
+                for r in res:
+                    f.write(r)
+            exists += len(batch)
 
-    reindex_S1()
-    print_and_log(f"Step 1 finished, in {time.time()-time1} s.")
+        reindex_S1()
+        print_and_log(f"Step 1 finished, in {time.time()-time1} s.")
+    else:
+        print_and_log(f"{region_name} S1Rj_reindex already exists")
     # --- Step 1 ---
 
+    # --- Step 2 ---
     hospitals = pd.read_csv("./data/tmp/S1Rj_reindex.csv")
     hospitals_gdf = gpd.GeoDataFrame(hospitals, geometry=gpd.points_from_xy(hospitals["lng"], hospitals["lat"]))
     hospitals_id = ray.put(hospitals_gdf)
 
-    # --- Step 2 ---
     print_and_log("Step 2 start ...")
     time2 = time.time()
 
@@ -255,6 +283,8 @@ def main(region_name, shape):
     print_and_log("----------------")
 
     # --- clean ---
+    check_and_remove(f"./data/tmp/acc_reindex.csv")
+    check_and_remove(f"./data/tmp/S1Rj_reindex.csv")
     check_and_remove(f"./data/tmp/{region_name}.tif")
     check_and_remove(f"./data/tmp/{region_name}.geojson")
     check_and_remove(f"./data/tmp/{region_name}_roads.osm.pbf")
@@ -344,7 +374,7 @@ def check_and_create(path, head):
 
 check_and_create("./data/tmp/logs.txt", "")
 
-for I, R in china.iloc[::-1].iterrows():
+for I, R in china.iterrows():
     region = gpd.GeoDataFrame([R])
     region = region.drop(columns=["adchar"])
     rn = region["name"].values[0]
@@ -387,7 +417,7 @@ for I, R in china.iloc[::-1].iterrows():
 
     try:
         ray.init()
-        main(rn, gpd.GeoDataFrame([R]))
+        process_region(rn, gpd.GeoDataFrame([R]))
     finally:
         p.terminate()
         p.wait()
